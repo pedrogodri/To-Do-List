bf32f8263a2b82e5924749385a1d9861b5d0aa65
Pedro Henrique Godri <pedrogodri@gmail.com>
2022-12-12 01:09:58 -0300

    Iniciando o JS

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Seleção de elementos
+const todoForm = document.querySelector('#todo-form');
+const todoInput = document.querySelector('#todo-input');
+const todoList = document.querySelector('#todo-list');
+const editForm = document.querySelector('#edit-form');
+const editInput = document.querySelector('#edit-input');
+const cancelEditBtn = document.querySelector('#cancel-edit-btn');
+
+
+// Funções
+const saveTodo = (text) => {
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    const todoTitle = document.createElement('h3');
+}
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputValue = todoInput.value
+    if (inputValue) {
+        saveTodo(inputValue)
+    }
+});
